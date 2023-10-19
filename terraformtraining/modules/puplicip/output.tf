0c176679092818dc178625ccae8ea79122c3746a
sasidharanshanmugam <104121907+sasidharanshanmugam@users.noreply.github.com>
2023-10-19 08:31:51 +0530

    Delete terraformtraining/modules directory

--- terraformtraining/modules/puplicip/output.tf
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-output "LBIp" {
-  value = azurerm_public_ip.example.id
-=======
-output "LBIp" {
-  value = azurerm_public_ip.example.id
->>>>>>> 1f28a3a34f47dfd6326a2f214d924c99c128e24a
-}
--- terraformtraining/modules/resourcegroup/output.tf
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-output "resourcegroupname" {
-  value = azurerm_resource_group.rg.name
-=======
-output "resourcegroupname" {
-  value = azurerm_resource_group.rg.name
->>>>>>> 1f28a3a34f47dfd6326a2f214d924c99c128e24a
-}
